56505d0bce53d6474972e3f927b80ee761b54137
bkress <bkress@it-economics.de>
2016-11-07 14:04:17 +0100

    Initial Commit

---
+++ HerbieNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HerbieNow
+//
+//  Created by Benno Kress on 07.11.16.
+//  Copyright © 2016 LMU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
