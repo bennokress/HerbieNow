0d74e25e02c0b6562fc4c7a0ff355ed51081e354
bkress <bkress@it-economics.de>
2016-11-13 18:25:37 +0100

    Protocol for MainViewController added

---
+++ HerbieNow/Filteroption.swift
@@ -0,0 +1,24 @@
+//
+//  Filteroption.swift
+//  HerbieNow
+//
+//  Created by Benno Kress on 13.11.16.
+//  Copyright © 2016 LMU. All rights reserved.
+//
+
+import Foundation
+
+enum Filteroption {
+    
+    // MARK: Providers
+    case driveNow
+    case car2go
+    
+    // MARK: Characteristics
+    case sportscar
+    case hifiUpgrade
+    case convertible
+    case electric
+    // TODO: Implement more filteroptions
+    
+}
--- HerbieNow/MainViewController.swift
+++ HerbieNow/MainViewController.swift
@@ -8,19 +8,53 @@
 
 import UIKit
 
+protocol MainViewControllerProtocol {
+    
+    // This Protocol contains every function, the presenter can call.
+    
+    /// Displays a checkmark at the given filterbutton. Typically after a previously deactivated button is pressed.
+    func activate(filter: Filteroption)
+    
+    /// Removes the checkmark from the given filterbutton. Typically after a previously activated button is pressed.
+    func deactivate(filter: Filteroption)
+    
+    /// Performs the segue to Map View. Typically after the search button is pressed.
+    func goToMapView()
+    
+    /// Performs the segue to Settings View. Typically after the settings button is pressed.
+    func goToSettingsView()
+    
+}
+
 /// ViewControllers have no logic other than what to display
 class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
+extension MainViewController: MainViewControllerProtocol {
+    
+    func activate(filter: Filteroption) {
+        <#code#>
+    }
+    
+    func deactivate(filter: Filteroption) {
+        <#code#>
+    }
+    
+    func goToMapView() {
+        <#code#>
+    }
+    
+    func goToSettingsView() {
+        <#code#>
+    }
+    
+}
