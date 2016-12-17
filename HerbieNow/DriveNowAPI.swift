4aa28a13e80e0c0575a87e21906369a38419ef7f
Benno Kress <bkress@it-economics.de>
2016-12-17 15:52:08 +0100

    removed unnecessary type extension

--- HerbieNow/DriveNowAPI.swift
+++ HerbieNow/DriveNowAPI.swift
@@ -9,16 +9,6 @@
 import Foundation
 import Alamofire
 
-extension String: ParameterEncoding {
-
-    public func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
-        var request = try urlRequest.asURLRequest()
-        request.httpBody = data(using: .utf8, allowLossyConversion: false)
-        return request
-    }
-
-}
-
 class DriveNowAPI {
 
     let keychain = KeychainService.shared
