08550e6d48e4f5306e850ff9e45de0de2e951508
Benno Kress <bkress@it-economics.de>
2016-12-21 15:56:50 +0100

    General Interpreter Protocol implemented to get easy communication from the Location Manager in AppDelegate to the Interpreter interpreting the Locations it receives

---
+++ HerbieNow/GeneralInterpreterProtocol.swift
@@ -0,0 +1,29 @@
+//
+//  GeneralInterpreterProtocol.swift
+//  HerbieNow
+//
+//  Created by Benno Kress on 21.12.16.
+//  Copyright © 2016 LMU. All rights reserved.
+//
+
+import Foundation
+
+//// General interpreter is used by AppDelegate to send Location Updates to the interpreter of the currently active view
+//protocol GeneralInterpretProtocol {
+//    
+//    var appDelegate: AppDelegate { get }
+//    
+//    func locationUpdated(_ location: Location)
+//    
+//    func requestRegularLocationUpdates()
+//    
+//}
+//
+//extension GeneralInterpretProtocol {
+//    
+//    func requestRegularLocationUpdates() {
+//        appDelegate.registerCurrentInterpreterForLocationUpdates(self) // tells AppDelegate where to send location updates
+//        appDelegate.locationManager.requestAlwaysAuthorization() // starts location updates (or displays alert for authorization)
+//    }
+//    
+//}
