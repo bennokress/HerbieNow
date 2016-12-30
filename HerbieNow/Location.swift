fcdbe472282741cf6f274c90bc1d046eba2bbfbd
Benno Kress <bkress@it-economics.de>
2016-12-30 18:33:32 +0100

    Reverse Geocoding now available for all addresses via getAddress() { (street, areaCode, city) in … }

--- HerbieNow/Location.swift
+++ HerbieNow/Location.swift
@@ -9,16 +9,12 @@
 import Foundation
 import CoreLocation
 
-class Location {
+struct Location {
 
     typealias AddressDictFormat = [String: Any]
 
     let latitude: Double
     let longitude: Double
-    
-    var city: String = ""
-    var street: String = ""
-    var areaCode: String = ""
 
     let coordinateDescription: String
 
@@ -32,60 +28,33 @@ class Location {
         self.latitude = latitude
         self.longitude = longitude
 
-        //        coordinateDescription = "lat: \(latitude), long: \(longitude)"
-        coordinateDescription = street + ", " + areaCode + ", " + city
-
-        //        self.street = getStreet(from: latitude, longitude)
-        //        self.areaCode = getAreaCode(from: latitude, longitude)
-        //        self.city = getCity(from: latitude, longitude)
-
-        getAdress { result in
-
-            guard let city = result?.city, let street = result?.street, let areaCode = result?.areaCode else {
-                return
-            }
-            self.city = city
-            self.street = street
-            self.areaCode = areaCode
-
-        }
+        coordinateDescription = "lat: \(latitude), long: \(longitude)"
     }
 
-    func getAdress(completion: @escaping ((street:String, areaCode:String, city:String)?) -> Void) {
+    func getAddress(completion: @escaping ((street: String, areaCode: String, city: String)?) -> Void) {
         let geocoder = CLGeocoder()
         geocoder.reverseGeocodeLocation(currentLocationAsObject) { (placemarks, error) in
             if error != nil {
-                print("error")
+                print(Debug.error(class: self, func: #function, message: "Reverse Geocoding failed"))
                 completion(nil)
             } else {
                 let placeArray = placemarks as [CLPlacemark]!
                 var placeMark: CLPlacemark!
                 placeMark = placeArray?[0]
+
                 guard let dict = placeMark.addressDictionary as? (AddressDictFormat) else {
                     return
                 }
-                
+
                 guard let city = dict["City"] as? String, let street = dict["Street"] as? String, let areaCode = dict["ZIP"] as? String else {
                     return
                 }
-                
-                self.city = city
-                self.areaCode = areaCode
-                self.street = street
+
+                print(Debug.success(class: self, func: #function, message: "Reverse Geocoding: (\(self.latitude), \(self.longitude)) is \(street) in \(areaCode) \(city)"))
                 completion((street, areaCode, city))
             }
 
         }
-    }
-
-    func getStreet(from lat: Double, _ long: Double) -> String {
-        // TODO: reverse Geocode
-        return ""
-    }
-
-    func getAreaCode(from lat: Double, _ long: Double) -> String {
-        // TODO: reverse Geocode
-        return ""
     }
 
 }
