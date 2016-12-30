c36eaf0124398e6e142c4581332386e80087b547
Benno Kress <bkress@it-economics.de>
2016-12-30 15:27:58 +0100

    New Debug Print Format implemented for all print statements

--- HerbieNow/MainViewPresenter.swift
+++ HerbieNow/MainViewPresenter.swift
@@ -73,7 +73,6 @@ extension MainViewPresenter: MainViewPresenterProtocol {
 
     func display(message: String) {
         // mainVC . do something
-        print("(MainViewPresenter -> display())" + message)
     }
 
 }
