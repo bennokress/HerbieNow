6073b1cee466e901374dabe7e6945d608e4ca29a
Benno Kress <bkress@it-economics.de>
2017-01-05 13:06:42 +0100

    Filterset struct added

---
+++ HerbieNow/Filterset.swift
@@ -0,0 +1,41 @@
+//
+//  Filterset.swift
+//  HerbieNow
+//
+//  Created by Benno Kress on 05.01.17.
+//  Copyright © 2017 LMU. All rights reserved.
+//
+
+import Foundation
+
+protocol FiltersetProtocol {
+    
+    func filter(vehicles: [Vehicle]) -> [Vehicle]
+    
+}
+
+struct Filterset {
+    
+    let filters: [Filter]
+    
+    init(from initString: String) {
+        filters = []
+    }
+    
+}
+
+extension Filterset: FiltersetProtocol {
+    
+    func filter(vehicles: [Vehicle]) -> [Vehicle] {
+        
+        var filteredVehicles: [Vehicle] = []
+        
+        for filter in filters {
+//            filteredVehicles.append(filter.vehicles(vehicles))
+        }
+        
+        return vehicles
+        
+    }
+    
+}
