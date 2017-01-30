674c5c2a2afe2bd2a932c3f1c495ea631e8d0784
Benno Kress <bkress@it-economics.de>
2017-01-30 15:24:33 +0100

    Prepared Code Structure for stricter MVP Design

--- HerbieNow/VehicleMapViewController.swift
+++ HerbieNow/VehicleMapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MapViewController.swift
+//  VehicleMapViewController.swift
 //  HerbieNow
 //
 //  Created by M Z on 30/01/2017.
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 import CoreLocation
 
-class MapViewController: UIViewController {
+class VehicleMapViewController: UIViewController {
     
     //cmd drag mit der map view im storyboard
     @IBOutlet weak var mapView: MKMapView!
@@ -52,7 +52,7 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
         centerMapOnLocation(location: initialLocation)
         mapView.delegate = self
-        var annotations: [MyAnnotation] = []
+        var annotations: [PinAnnotation] = []
         for vehicle in getVehicles() {
             var color: UIColor
             if vehicle.provider == "DriveNow" {
@@ -60,7 +60,7 @@ class MapViewController: UIViewController {
             }else{
                 color = UIColor.blue
             }
-            let anno = MyAnnotation(title: "Car", locationName: vehicle.getDescription(), discipline: "Car", coordinate: vehicle.location, color: color)
+            let anno = PinAnnotation(title: "Car", locationName: vehicle.getDescription(), discipline: "Car", coordinate: vehicle.location, color: color)
             annotations.append(anno)
         }
         mapView.addAnnotations(annotations)
@@ -69,3 +69,47 @@ class MapViewController: UIViewController {
     
 }
 
+extension VehicleMapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if let annotation = annotation as? PinAnnotation {
+            let identifier = "pin"
+            var view: MKPinAnnotationView
+            if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView {
+                dequeuedView.annotation = annotation
+                view = dequeuedView
+            } else {
+                view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                let coloredAnnotation = annotation
+                view.pinTintColor = coloredAnnotation.color
+                view.canShowCallout = true
+                view.calloutOffset = CGPoint(x: -5, y: 5)
+                /*
+                 let calloutButton = UIButton(type: .custom)
+                 calloutButton.frame = CGRect(x: 0, y: 0, width: 60, height: 30)
+                 calloutButton.layer.borderWidth = 1
+                 calloutButton.layer.borderColor = UIColor.black.cgColor
+                 calloutButton.setTitle("Reservieren", for: .normal)
+                 */
+                view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure) as UIView
+            }
+            return view
+        }
+        return nil
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        // Action for pressing on "info-Button"
+        print("Button tapped")
+        if let annot = view.annotation as? PinAnnotation {
+            // alert window
+            let ac = UIAlertController(title: "Hier kann man nun reservieren",
+                                       message: annot.locationName,
+                                       preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+        
+    }
+    
+}
