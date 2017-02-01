dbd4951d9851d296b19c59b20fb29d9192bca75b
M Z <mzmacuni@dave-vm.mmk.e-technik.tu-muenchen.de>
2017-02-01 19:33:27 +0100

    Pin Annotation View displays the correct strings now

--- HerbieNow/PinAnnotation.swift
+++ HerbieNow/PinAnnotation.swift
@@ -12,15 +12,15 @@ import MapKit
 class PinAnnotation: NSObject, MKAnnotation {
     let title: String?
     let verhicleDescription: String
-    let distanceBonus: String
+    let fuelInfo: String
     let distanceUser: String
     let coordinate: CLLocationCoordinate2D
     let color: UIColor
 
-    init(title: String, vehicleDescription: String, distanceBonus: String, distanceUser: String, coordinate: CLLocationCoordinate2D, color: UIColor) {
+    init(title: String, vehicleDescription: String, fuelInfo: String, distanceUser: String, coordinate: CLLocationCoordinate2D, color: UIColor) {
         self.title = title
         self.verhicleDescription = vehicleDescription
-        self.distanceBonus = distanceBonus
+        self.fuelInfo = fuelInfo
         self.distanceUser = distanceUser
         self.coordinate = coordinate
         self.color = color
